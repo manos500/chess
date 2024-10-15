066d57e9306661109d78d45021be4445e2baa53e
Manos <manosnikitakis3@gmail.com>
2024-10-15 18:44:30 +0300

    add pieces on board

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,59 @@
-import { useState } from 'react'
+import { bb, br, bn, bq, bp, bk, wb, wk, wn, wp, wq, wr } from './assets'
 import './style.css'
 
 const App = () => {
   return (
     <div class="chess-board">
   
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
+        <div class="white">
+          <img src={br} alt="" />
+        </div>
+        <div class="black">
+          <img src={bn} alt="" />
+        </div>
+        <div class="white">
+          <img src={bb} alt="" />
+        </div>
+        <div class="black">
+          <img src={bq} alt="" />
+        </div>
+        <div class="white">
+          <img src={bk} alt="" />
+        </div>
+        <div class="black">
+          <img src={bb} alt="" />
+        </div>
+        <div class="white">
+          <img src={bn} alt="" />
+        </div>
+        <div class="black">
+          <img src={br} alt="" />
+        </div>
 
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
+        <div class="black">
+          <img src={bp} alt="" />
+        </div>
+        <div class="white">
+          <img src={bp} alt="" />
+        </div>
+        <div class="black">
+          <img src={bp} alt="" />
+        </div>
+        <div class="white">
+          <img src={bp} alt="" />
+        </div>
+        <div class="black">
+          <img src={bp} alt="" />
+        </div>
+        <div class="white">
+          <img src={bp} alt="" />
+        </div>
+        <div class="black">
+          <img src={bp} alt="" />
+        </div>
+        <div class="white">
+          <img src={bp} alt="" />
+        </div>
      
         <div class="white"></div>
         <div class="black"></div>
@@ -59,23 +91,55 @@ const App = () => {
         <div class="black"></div>
         <div class="white"></div>
      
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
+        <div class="white">
+          <img src={wp} alt="" />
+        </div>
+        <div class="black">
+          <img src={wp} alt="" />
+        </div>
+        <div class="white">
+          <img src={wp} alt="" />
+        </div>
+        <div class="black">
+          <img src={wp} alt="" />
+        </div>
+        <div class="white">
+          <img src={wp} alt="" />
+        </div>
+        <div class="black">
+          <img src={wp} alt="" />
+        </div>
+        <div class="white">
+          <img src={wp} alt="" />
+        </div>
+        <div class="black">
+          <img src={wp} alt="" />
+        </div>
  
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
-        <div class="black"></div>
-        <div class="white"></div>
+        <div class="black">
+          <img src={wr} alt="" />
+        </div>
+        <div class="white">
+          <img src={wn} alt="" />
+        </div>
+        <div class="black">
+          <img src={wb} alt="" />
+        </div>
+        <div class="white">
+          <img src={wq} alt="" />
+        </div>
+        <div class="black">
+          <img src={wk} alt="" />
+        </div>
+        <div class="white">
+          <img src={wb} alt="" />
+        </div>
+        <div class="black">
+          <img src={wn} alt="" />
+        </div>
+        <div class="white">
+          <img src={wr} alt="" />
+        </div>
     </div>
   )
 }
