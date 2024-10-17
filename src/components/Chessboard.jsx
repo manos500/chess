f0629e0759316fa905cb73e5ae5f4aed35fb1f09
Manos <manosnikitakis3@gmail.com>
2024-10-17 12:04:16 +0300

    add grab move and drop functions

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -6,9 +6,12 @@ const row = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const column = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 const pieces = [];
+
 for (let i=0; i < 8; i++) {
   pieces.push({image: wp, x: i, y: 1})
-  pieces.push({image: bp, x: i, y: 6})}
+  pieces.push({image: bp, x: i, y: 6})
+}
+
 pieces.push({image: wr, x: 0, y: 0})
 pieces.push({image: wr, x: 7, y: 0})
 pieces.push({image: wn, x: 1, y: 0})
@@ -25,6 +28,48 @@ pieces.push({image: bb, x: 2, y: 7})
 pieces.push({image: bb, x: 5, y: 7})
 pieces.push({image: bk, x: 4, y: 7})
 pieces.push({image: bq, x: 3, y: 7})
+
+let activepiece;
+
+function clickPiece(e) {
+
+}
+
+function dropPiece(e) {
+  if(activepiece){
+    activepiece = null;
+  } 
+}
+
+function movePiece(e) {
+   if (activepiece){
+
+      const mouseX = e.clientX - 50;
+      const mouseY = e.clientY - 50;
+
+      activepiece.style.position = "absolute"
+      activepiece.style.left = `${mouseX}px`;
+      activepiece.style.top = `${mouseY}px`;
+
+  }
+}
+
+
+function grabPiece(e) {
+  if (e.target.classList.contains("chess-piece")){
+      console.log(e.target)
+      const mouseX = e.clientX - 50;
+      const mouseY = e.clientY - 50;
+
+      e.target.style.position = "absolute"
+      e.target.style.left = `${mouseX}px`;
+      e.target.style.top = `${mouseY}px`;
+
+      activepiece = e.target
+
+  }
+
+}
 
 export const Chessboard = () => {
     let board = [];
@@ -45,6 +90,6 @@ export const Chessboard = () => {
 
 
   return (
-    <div className='chessboard'>{board}</div>
+    <div onClick={e => clickPiece(e)} onMouseUp={e => dropPiece(e)} onMouseMove={e => movePiece(e)} onMouseDown={e => grabPiece(e)} className='chessboard'>{board}</div>
   )
 }
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -4,11 +4,11 @@ import './tile.css'
 export default function Tile ( {number, image} ) {
     if ( number % 2 === 0){
         return <div className='tile black-tile'>
-            <div style = {{backgroundImage : `url(${image})`}} className='chess-piece'></div>
+            {image && <div style = {{backgroundImage : `url(${image})`}} className='chess-piece'></div>}
         </div>
     }else {
         return <div className='tile white-tile'>
-            <div style = {{backgroundImage : `url(${image})`}} className='chess-piece'></div>
+            {image && <div style = {{backgroundImage : `url(${image})`}} className='chess-piece'></div>}
         </div>
     }
   
